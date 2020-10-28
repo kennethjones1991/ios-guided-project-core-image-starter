f6a5890e983581510297dd02c15fde6c30d8c935
Kenneth Jones <kennethjones1991@outlook.com>
2020-10-27 21:03:29 -0600

    Improved performance by scaling the image before sending it to CoreImage

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
@@ -8,17 +9,89 @@ class PhotoFilterViewController: UIViewController {
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
+    
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else {
+                scaledImage = nil
+                return
+            }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = imageView.contentScaleFactor // alternatively you could use UIScreen.main.scale
+//            let scale: CGFloat = 0.5 // Makes image blurry but performance is amazing
+            
+            scaledSize.width *= scale
+            scaledSize.height *= scale
+            
+            let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize)
+            
+            scaledImage = scaledUIImage
+        }
+    }
+    
+    var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let context = CIContext()
+    private let filter = CIFilter.colorControls()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+//        let filter = CIFilter.gaussianBlur()
+//        let filter2 = CIFilter(name: "CIColorControls")
+//
+//        print(filter.attributes)
+        
+        originalImage = imageView.image
 	}
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("The photo library is not available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker,animated: true, completion: nil)
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        let inputImage = CIImage(image: image)
+        
+        filter.inputImage = inputImage
+        filter.saturation = saturationSlider.value
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        
+        guard let outputImage = filter.outputImage else { return image }
+        
+        guard let renderedCGImage = context.createCGImage(outputImage, from: outputImage.extent) else { return image }
+        
+        return UIImage(cgImage: renderedCGImage)
+    }
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
 	
 	// MARK: Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
-		// TODO: show the photo picker so we can choose on-device photos
+		// Show the photo picker so we can choose on-device photos
 		// UIImagePickerController + Delegate
+        presentImagePickerController()
 	}
 	
 	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
@@ -29,15 +102,30 @@ class PhotoFilterViewController: UIViewController {
 	// MARK: Slider events
 	
 	@IBAction func brightnessChanged(_ sender: UISlider) {
-
+        updateImage()
 	}
 	
 	@IBAction func contrastChanged(_ sender: Any) {
-
+        updateImage()
 	}
 	
 	@IBAction func saturationChanged(_ sender: Any) {
-
+        updateImage()
 	}
 }
 
+extension PhotoFilterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
